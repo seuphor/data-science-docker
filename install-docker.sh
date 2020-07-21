9b7d12454a3d5acd6e1042e81115db0e3e64d2ef
Zihao Ji <aiweiwei@Zihaos-MBP-2.fios-router.home>
2020-07-21 00:55:47 -0400

    [Add]: add docker-compose.yml for metabase

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y remove docker docker-engine docker.io
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-compose
+
+sudo systemctl restart docker
+sudo systemctl enable docker
+
+# fix permission issue
+sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 666 /var/run/docker.sock
